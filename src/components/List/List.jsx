53d5f74ed9a598881b9404cca7f24761f186e11b
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2021-08-25 12:51:29 +0530

    updated Readme.md

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  FormControl,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
+import useStyles from "./styles";
 
 const List = () => {
-  return <h1>List</h1>;
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants , Hotels & Attractionsaround you.
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={} onChange={}>
+          <MenuItem value="restaurant">Restaurants</MenuItem>
+          <MenuItem value="hotel">Hotels</MenuItem>
+          <MenuItem value="attraction">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 export default List;
